LineageOS/android_hardware_qcom_display

sdm: add board flag for panel shift
Change-Id: I0939619506b82d75a5db985dabc9297142851efe

sdm/libs/core/fb/hw_device.cpp
@@ -243,6 +243,13 @@ DisplayError HWDevice::Validate(HWLayers *hw_layers) {
         mdp_layer.horz_deci = pipe_info->horizontal_decimation;
         mdp_layer.vert_deci = pipe_info->vertical_decimation;
 
+#ifdef DISPLAY_SHIFT_X
+        if (device_type_ == kDevicePrimary) {
+          pipe_info->dst_roi.left += DISPLAY_SHIFT_X;
+          pipe_info->dst_roi.right += DISPLAY_SHIFT_X;
+        }
+#endif
+
         SetRect(pipe_info->src_roi, &mdp_layer.src_rect);
         SetRect(pipe_info->dst_roi, &mdp_layer.dst_rect);
         SetMDPFlags(&layer, is_rotator_used, is_cursor_pipe_used, &mdp_layer.flags);

TrololoBird/android_hardware_qcom_display-caf-msm8998

sdm/libs/core/fb/hw_device.cpp
@@ -249,6 +249,21 @@ DisplayError HWDevice::Validate(HWLayers *hw_layers) {
 #ifdef MDP_COMMIT_RECT_NUM
         mdp_layer.rect_num = pipe_info->rect;
 #endif
+
+#ifdef DISPLAY_SHIFT_HORIZONTAL
+        if (device_type_ == kDevicePrimary) {
+          pipe_info->dst_roi.left += DISPLAY_SHIFT_HORIZONTAL;
+          pipe_info->dst_roi.right += DISPLAY_SHIFT_HORIZONTAL;
+        }
+#endif
+
+#ifdef DISPLAY_SHIFT_VERTICAL
+        if (device_type_ == kDevicePrimary) {
+          pipe_info->dst_roi.top += DISPLAY_SHIFT_VERTICAL;
+          pipe_info->dst_roi.bottom += DISPLAY_SHIFT_VERTICAL;
+        }
+#endif
+
         SetRect(pipe_info->src_roi, &mdp_layer.src_rect);
         SetRect(pipe_info->dst_roi, &mdp_layer.dst_rect);
         SetMDPFlags(&layer, is_rotator_used, is_cursor_pipe_used, &mdp_layer.flags);